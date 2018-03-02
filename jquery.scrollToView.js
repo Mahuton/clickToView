6ba041df3fc5f72f3646ef11160aa6d1775a4845
Mahuton <bpossoupe@gmail.com>
2018-03-02 12:45:45 +0100

    updating js file

--- jquery.scrollToView.js
+++ jquery.scrollToView.js
@@ -16,10 +16,6 @@
       {
         $(this).click(function()
           {
-          //alert("good");
-          //$vv = $(params.target).offset().top;
-          //alert($vv);
-          //$(params.target).css({'border':'solid red 2px'});
             $('html,body').animate(
               {
                 scrollTop:$(settings.target).offset().top-settings.viewLevel
